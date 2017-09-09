37aafe6676955c221ae52688de91dbef93f16b4c
ArsalaanAnsariDeveloper <aaa2325@columbia.edu>
2017-09-09 12:10:58 -0400

    Added marker capabilities

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,24 +1,88 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity} from 'react-native';
 import {MapView} from 'expo';
+
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+let LATITUDE = 37.78825;
+let LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.001;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
+
+function randomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 export default class Maps extends Component {
-  render() {
+
+    
+    
+  
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+         
+          markers: [],
+        };
+      }
+    
+      onMapPress(e) {
+        this.setState({
+          markers: [
+            ...this.state.markers,
+            {
+              coordinate: e.nativeEvent.coordinate,
+              key: id++,
+              color: randomColor(),
+            },
+          ],
+        });
+      }
+      
+  
+    render() {
       let lat = Number(this.props.latitude);
       let long = Number(this.props.longitude);
      
     return (
       <View style={styles.container}>
-      <View style={styles.map}>
-      <MapView
-        style={{ flex: 1 }}
+      
+      <MapView 
+        style={styles.map}
         initialRegion={{
-          latitude: lat,
+            latitude: lat,
           longitude: long,
           latitudeDelta: 0.001,
           longitudeDelta: 0.001,
-        }}
-        />
-        </View>
+        }
+            
+        }
+        onPress={(e) => this.onMapPress(e)}
+        
+
+    >
+    {this.state.markers.map(marker => (
+            <MapView.Marker
+              key={marker.key}
+              coordinate={marker.coordinate}
+              pinColor={marker.color}
+            />
+          ))}
+    </MapView>
+
+          <View style={styles.buttonContainer}> 
+          <TouchableOpacity
+            onPress={() => this.setState({ markers: [] })}
+            style={styles.bubble}
+          >
+            <Text>Clear Tags</Text>
+          </TouchableOpacity>
+        
+          </View>
+        
       
       </View>
     );
@@ -41,5 +105,16 @@ const styles = StyleSheet.create({
       left: 0,
       right: 0,
       bottom: 0,
+    },
+    bubble: {
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20,
+      },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        backgroundColor: 'transparent',
     },
   });
